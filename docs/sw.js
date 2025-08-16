2009af5097bcb079941396567178f4dfb7ce7eb5
motyarbel2021 <motyarbel2021@gmail.com>
2025-08-16 20:38:43 +0300

    Add files via upload

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Hebrew Transportation Coordination App
-const CACHE_NAME = 'hebrew-transport-app-v1.0.6-scroll-test';
+const CACHE_NAME = 'hebrew-transport-app-v1.0.7-fixed';
 const OFFLINE_URL = './offline.html';
 
 // Files to cache for offline functionality
